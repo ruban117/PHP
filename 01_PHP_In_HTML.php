a8355df62a2a81f193a97bc2fef34ff55bf20d84
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-29 10:30:52 +0530

    Add files via upload

---
+++ 01_PHP_In_HTML.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "This Is "; echo "Title";?></title>
+</head>
+<body>
+    <h1><?php echo "Hello!"; ?></h1>
+</body>
+</html>
