64a61c5c134c41c6913d44616b6acb12ec177d7e
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-29 12:31:05 +0530

    Add files via upload

---
+++ 03_Data_Types_PHP.php
@@ -0,0 +1,40 @@
+<?php
+    //string datatype
+    $name='Ruban <br>';
+    echo "$name <br>";
+    echo var_dump($name),"<br>";
+
+    //Integer Datatype
+    $sal=10000;
+    echo $sal,"<br>";
+    echo var_dump($sal),"<br>";
+
+    //Float Datatype
+    $sal=10000.328;
+    echo $sal,"<br>";
+    echo var_dump($sal),"<br>";
+
+    //Boolean
+
+    $x=true;
+    $y=false;
+    echo $x,"<br>";
+    echo var_dump($x),"<br>";
+    echo $y,"<br>";
+    echo var_dump($y),"<br>";
+
+    //Array Data type
+
+    $arr=array("Ruban",117,"Soumita",96);
+    echo var_dump($arr),"<br>";
+    echo $arr[0],"<br>";
+    echo $arr[1],"<br>";
+    echo $arr[2],"<br>";
+    echo $arr[3],"<br>";
+
+    //Null data type
+
+    $d=NULL;
+    echo $d,"<br>";
+    echo var_dump($d);
+?>
