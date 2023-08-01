c0041c913dfafc5caf5aea72f338ff46b78ea13a
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-08-01 21:51:49 +0530

    Add files via upload

---
+++ 10_For_Each_Loop_PHP.php
@@ -0,0 +1,12 @@
+<?php
+    $arr=array("Ruban","Soumita","Souvik","Susmita");
+    for($i=0;$i<count($arr);$i++){
+        echo $arr[$i] . "<br>";
+    }
+
+    //using for each loop
+    
+    foreach($arr as $val){
+        echo $val . "<br>";
+    }
+?>
