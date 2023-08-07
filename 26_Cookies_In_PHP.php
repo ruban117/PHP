9918d1731ba4af2aa1bdc85e402de0e9fd5de6e2
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-08-07 12:36:29 +0530

    Update 26_Cookies_In_PHP.php

--- 26_Cookies_In_PHP.php
+++ 26_Cookies_In_PHP.php
@@ -1,7 +1,7 @@
 <?php
     echo "Welcome to the setting of coockies!<br>";
 
-    setcookie("cookie","Books",time() + 86400,"/");
+    setcookie("cookie","Books",time() + 86400,"/");//cookie_name,cookie_value,_expire_time 86400=24hrs,/means throughout website
 
     echo "Your Cookie Has Been set successfully!";
 ?>
