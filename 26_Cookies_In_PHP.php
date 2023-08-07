3528c72fb2ac237e925f4e272decb02daff8d51b
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-08-07 12:39:10 +0530

    Update 26_Cookies_In_PHP.php

--- 26_Cookies_In_PHP.php
+++ 26_Cookies_In_PHP.php
@@ -1,4 +1,6 @@
 <?php
+
+    //coolie means some kind of information which is not necerrary for hackers but necessary for you to identify a particular user have to store it into your browser for a cretain period of time
     echo "Welcome to the setting of coockies!<br>";
 
     setcookie("cookie","Books",time() + 86400,"/");//cookie_name,cookie_value,_expire_time 86400=24hrs,/means throughout website
